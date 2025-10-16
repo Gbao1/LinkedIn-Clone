24fc0cbe3724eb882bb04983984189f1b8a6dd3f
Lornng <ngjianjia02@gmail.com>
2025-10-16 16:50:44 +1100

    update YAML pipeline with local host

--- Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35a78d4dc753ff50c12986d7663c39080f36353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5168fe82d77e621d9d1e67755ad8b92bfee0ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
